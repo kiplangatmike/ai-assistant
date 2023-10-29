eb0a82d786ccdce80fa6d01b5e70a0e6b1bf6bbe
kiplangatmike <m.kiplangat@alustudent.com>
2023-10-29 10:38:27 +0200

    feat: card components

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { ChatBox } from "@/components/ui/chat/chatBox"
+
+export default function Home() {
+  return (
+    <section>
+      <div className="flex items-center justify-center">
+        <ChatBox />
+      </div>
+    </section>
+  )
+}
