34a97aad0a8a6d600ebb9365209916fbd612ea20
kiplangatmike <m.kiplangat@alustudent.com>
2023-10-29 18:24:34 +0200

    feat: updating open ai version

--- app/api/test_api/route.ts
+++ app/api/test_api/route.ts
@@ -1,6 +1,6 @@
 // app/api/test_api/route.ts
 import { NextRequest } from "next/server"
-import { Configuration, OpenAIApi } from "openai"
+import OpenAI from "openai"
 
 import formatMessages from "../../../helpers/formatMessages"
 
@@ -9,19 +9,21 @@ export async function GET(request: NextRequest) {
   const key = searchParams.get("key")
   const PROMPT = `Respond "Pong"`
 
-  const formatted = formatMessages(["ping:"], [], PROMPT)
-  let configuration = new Configuration({
+  // const formatted = formatMessages(["ping:"], [], PROMPT)
+  // let configuration = new Configuration({
+  //   apiKey: key as string,
+  // })
+  const openai = new OpenAI({
     apiKey: key as string,
   })
-  const openai = new OpenAIApi(configuration)
 
   try {
-    const completion = await openai.createChatCompletion({
+    const completion = await openai.chat.completions.create({
       model: "gpt-3.5-turbo",
-      messages: formatted,
+      messages: [{ role: "user", content: "Hello!" }],
     })
 
-    if (completion.data.choices && completion.data.choices[0]) {
+    if (completion.choices && completion.choices[0]) {
       return new Response(JSON.stringify({ status: true }), { status: 200 })
     } else {
       return new Response(
